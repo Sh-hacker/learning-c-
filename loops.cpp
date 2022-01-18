a8f63fc45c115918c77bcc13d003c3bae0dbd6da
shaurya agrawal <shauryaagrawal756@gmail.com>
2022-01-18 17:33:37 +0000

    initial commit

---
+++ loops.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int x;
+    cin>>x;
+
+    while (x>0)
+    {
+        cout<<x<<endl;
+        cin>>x;
+    }
+    
+    /*for(int counter=1;counter<=x;counter++){
+        sum=sum+counter;
+    }*/
+
+   // cout<<sum<<endl
+
+    return 0;
+
+
+
+}
